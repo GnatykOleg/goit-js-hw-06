3f04972a77a1b8612ec477508e6694b620856eb2
Oleg Gnatyk <103590840+yavenir@users.noreply.github.com>
2022-07-27 18:37:58 +0300

    Remove comments from js files

--- js/task-05.js
+++ js/task-05.js
@@ -1,9 +1,3 @@
-// Напиши скрипт который
-// при наборе текста в инпуте input#name - input(событие input)
-// подставляет его текущее значение в span#name - output.
-// Если инпут пустой,
-// в спане должна отображаться строка "Anonymous".
-
 const inputNameEl = document.querySelector('#name-input');
 const spanOutputNameEl = document.querySelector('#name-output');
 
--- js/task-09.js
+++ js/task-09.js
@@ -2,10 +2,6 @@ function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
 
-// Напиши скрипт, который изменяет цвета фона элемента < body >
-// через инлайн стиль при клике на button.change - color
-// и выводит значение цвета в span.color.
-
 const body = document.querySelector('body');
 const btn = document.querySelector('.change-color');
 const span = document.querySelector('.color');
